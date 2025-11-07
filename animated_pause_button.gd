f084095c4d5c0f7d0ac0fa20543d4407deb5e1b3
markariagno22 <Mark.ariagno14@gmail.com>
2025-11-07 16:51:48 -0600

    added scroll sound effect

--- animated_pause_button.gd
+++ animated_pause_button.gd
@@ -3,14 +3,16 @@ extends Button
 @onready var highlight = $ColorRect
 @onready var label = $CenterContainer
 @onready var anim = $AnimationPlayer
+@onready var scroll_click: AudioStreamPlayer = $"../../../ScrollClick"
+
 
 func _ready():
-	print("ready")
 	highlight.modulate.a = 0
 	label.scale = Vector2.ONE
-
+	
 func _on_mouse_entered():
 	anim.play("hover_in")
+	scroll_click.play()
 
 func _on_mouse_exited():
 	anim.play("hover_out")

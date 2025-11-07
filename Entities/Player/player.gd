9881913984e5ec8a6161710d74a2421e11098a6e
JPrei20 <joeprei20@gmail.com>
2025-11-06 18:14:17 -0600

    Created Player Movement
    The player is a square that can move in all 4 directions.

---
+++ Entities/Player/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+@export var SPEED : float = 300.0
+
+
+func _physics_process(_delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var horizontal_direction := Input.get_axis("move_left", "move_right")
+	var vertical_direction := Input.get_axis("move_up", "move_down")
+	if horizontal_direction or vertical_direction:
+		velocity.x = horizontal_direction * SPEED
+		velocity.y = vertical_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()

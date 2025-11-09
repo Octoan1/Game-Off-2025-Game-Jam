d3a8179c00c86ad6c193b138e40d79ca7f7edaae
Connradical <conneremoore@gmail.com>
2025-11-09 16:01:58 -0600

    Placeholder World Map
    Placeholder world map functionality implemented. Buttons and a map. Buttons do nothing as i have no player to work with yet.

---
+++ world_map_function.gd
@@ -0,0 +1,20 @@
+extends TextureRect
+
+	# move player / camera to that location on the map OR
+	# load the room scene 
+	# AND close the world map screen
+func lower_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func right_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func left_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func up_button_pressed() -> void:
+	pass # Replace with function body.
+	

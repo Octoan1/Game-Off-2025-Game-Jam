8daa8d2afa743f86aa740f6348e4c994a7aab1cf
goamek <goamek24@gmail.com>
2025-11-16 15:22:37 -0600

    done with the basic functionality after 8 long years of grinding in the trenches

--- AutoLoads/dialogue_state_manager.gd
+++ AutoLoads/dialogue_state_manager.gd
@@ -1,42 +1,50 @@
 extends Node 
 
-# Stores ALL game data: NPC_Name -> {emotion_scores, relationship_level, has_item}
 var npc_data: Dictionary = {}
-var result: String = ""
 
 # Template for new NPC data
 const NPC_TEMPLATE = {
 	"emotion_scores": {"Angry": 0, "Happy": 0, "Fearful": 0, "Sad": 0},
 	"relationship_level": 0,
-	"current_emotion": "Neutral" # Added for your daily emotion system
+	"current_emotion": ""
 }
 
-func initialize_npc(npc_name: String):
+func initialize_npc(npc_name: String, npc_emotion: String):
 	# Initializes data for a new NPC if it doesn't exist
 	if not npc_data.has(npc_name):
-		# We use deep_copy() to make sure each NPC gets a unique copy of the dictionary
 		npc_data[npc_name] = NPC_TEMPLATE.duplicate(true)
-		print("Initialized data for NPC: " + npc_name)
-
-# --- Core Function for Dialogue Manager ---
+		npc_data[npc_name]["current_emotion"] = npc_emotion
+		print("Initialized data for NPC: " + npc_name + ", " + npc_emotion)
 
 func add_emotion_score(npc_name: String, emotion_name: String, amount: int):
-	# 1. Ensure the NPC exists in the data structure
-	if not npc_data.has(npc_name):
-		initialize_npc(npc_name)
-	# 2. Add the score
 	var scores = npc_data[npc_name]["emotion_scores"]
 	scores[emotion_name] += amount
 	print(npc_name + ": " + emotion_name + " score increased to: " + str(scores[emotion_name]))
 	print(npc_data)
 
 func determine_highest_emotion(npc_name: String):
-	# Resets the scores for the current conversation and returns the result
 	var scores = npc_data[npc_name]["emotion_scores"]
+	var highest_emotion: String = "Neutral"
+	var highest_score: int = -1
 	
-	# ... (Your logic to determine highest emotion goes here)
-	# After determining the highest emotion (e.g., 'Angry'):
-	npc_data[npc_name]["current_emotion"] = "Angry"
+	# Loop through all emotion categories
+	for emotion_name in scores.keys():
+		var current_score = scores[emotion_name]
+		
+		if current_score > highest_score:
+			highest_score = current_score
+			highest_emotion = emotion_name
+			
+		elif current_score == highest_score and highest_score > 0:
+			# TODO: TIEBREAKER?
+			highest_emotion = "Mixed"
+	
+	# Store the final result in the NPC's data for printing in the dialogue
+	npc_data[npc_name]["current_emotion"] = highest_emotion
+	print("The emotion with the highest amount of points is: " + highest_emotion)
+	
+	return highest_emotion
+
 
 func reset_scores(npc_name: String):
 	# Reset scores for the next conversation
--- Dialogue/actionable.gd
+++ Dialogue/actionable.gd
@@ -1,9 +1,38 @@
 extends Area2D
 
-
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
 
-# runs the dialogue using the dialogue resource attached to the scene
+# You don't need to define npc_node/name/emotion here.
+
+# --- Nested Class for Game State ---
+# This class needs a constructor (_init) to accept the data
+# it should hold, instead of trying to access variables from the
+# outer script's top level.
+class NPCGameState:
+	var name: String
+	var emotion: String
+	
+	# Constructor to set the values when the object is created
+	func _init(npc_name: String, npc_emotion: String):
+		self.name = npc_name
+		self.emotion = npc_emotion
+# ------------------------------------
+
 func action() -> void:
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
+	# Get the parent node and its data inside the function
+	var npc_node = get_parent()
+	
+	# Safely retrieve the current values from the parent (assuming they exist)
+	var current_name = npc_node.npc_name
+	var current_emotion = npc_node.npc_emotion
+
+	# Create the NPCGameState instance, passing the current data to its constructor
+	var npc_state = NPCGameState.new(current_name, current_emotion)
+	
+	# Call the DialogueManager
+	DialogueManager.show_example_dialogue_balloon(
+		dialogue_resource, 
+		dialogue_start,
+		[self, { "npc" = npc_state }] # Using "npc" as the key
+	)
